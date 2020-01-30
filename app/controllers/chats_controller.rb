3bed7788136c559f22fde51294d740df7111e618
Guillaume Cabanel <guillaume.cabanel@gmail.com>
2020-01-30 19:47:18 +0100

    responsive design

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -6,20 +6,24 @@ class ChatsController < ApplicationController
   end
 
   def create
-    @chat = Chat.new(chat_params)
-    
-    if @chat.save
-      @chat.update(title: @chat.raw_chat.filename.base)
+    if params[:chat].present?
+      @chat = Chat.new(chat_params)
+      
+      if @chat.save
+        @chat.update(title: @chat.raw_chat.filename.base)
 
-      ::SetStatsFromRawChatService.new(@chat).call
+        ::SetStatsFromRawChatService.new(@chat).call
 
-      session[:last_chats] ||= []
-      session[:last_chats].insert(0, @chat.uuid)
+        session[:last_chats] ||= []
+        session[:last_chats].insert(0, @chat.uuid)
 
-      redirect_to chat_path @chat.uuid
-    else
-      render :new
+        return redirect_to chat_path @chat.uuid
+      end
     end
+    
+    @chat = Chat.new
+    @last_chats = session[:last_chats].present? ? Chat.where(uuid: session[:last_chats]) : []
+    render :new
   end
 
   def show
@@ -30,9 +34,9 @@ class ChatsController < ApplicationController
   end
 
   def destroy
-    @chat = Chat.find_by uuid: params[:id]
-    @chat.destroy
-
+    uuid = params[:id]
+    session[:last_chats] ||= []
+    session[:last_chats].delete(uuid)
     redirect_to new_chat_path
   end
 
